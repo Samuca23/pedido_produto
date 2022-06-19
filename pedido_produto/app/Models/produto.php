42fa5689138e2b4f8d41b98a9e914fc5c3226f2a
Samuca23 <samuelchiodini@hotmail.com>
2022-06-19 10:17:56 -0300

    começendo

--- pedido_produto/app/Models/produto.php
+++ pedido_produto/app/Models/produto.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class produto extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'procodigo',
+        'pronome', 
+        'prodescricao',
+        'provalor'
+    ];
 }
